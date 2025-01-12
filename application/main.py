21d6ffc0a00bd60a4ef45cdd9fb67b95c5e2f15f
Antoine Brunetti <antoine.brunetti@insee.fr>
2025-01-12 21:09:27 +0100

    🎉 BACKEND: Initialisation du projet et premier backend fonctionnel

---
+++ application/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from application.dao.configuration.database_connector import DatabaseConnector
+from application.web.user_router import user_router
+app = FastAPI()
+
+app.include_router(user_router)
+if __name__ == "__main__":
+    import uvicorn
+    # initialisation du connector
+    database_connector = DatabaseConnector("sqlite","default.db")
+    # initialisation de la bdd : schema et donnees
+    database_connector.init_db()
+    # Run server
+    uvicorn.run(app, host="0.0.0.0", port=8000)
