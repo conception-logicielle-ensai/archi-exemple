00ea28459d4d191117f853bffe1a6955c1608b36
Antoine Brunetti <antoine.brunetti@insee.fr>
2025-01-12 23:58:21 +0100

    ajout test auto et test unitaire avec magicmock

--- application/business_object/user.py
+++ application/business_object/user.py
@@ -6,9 +6,12 @@ class User:
         self.id = id
         self.username = username
         self.roles = roles
+
     def __str__(self):
         return f"User(username={self.username},roles={self.roles})"
+
     def __repr__(self):
         return f"User(username={self.username}, roles={self.roles})"
+
     def is_admin(self):
         return "admin" in self.roles
--- application/dto/user_dto.py
+++ application/dto/user_dto.py
@@ -1,13 +1,11 @@
 from pydantic import BaseModel, Field
+
 from application.business_object.user import User
 
 
 class UserDTO(BaseModel):
-    username: str = Field(examples=["adm","pasadm"])
+    username: str = Field(examples=["adm", "pasadm"])
+
     def to_user(self):
         """Convert DTO to a plain User object."""
-        return User(
-            id=None,
-            username=self.username,
-            roles=None
-        )
+        return User(id=None, username=self.username, roles=None)
