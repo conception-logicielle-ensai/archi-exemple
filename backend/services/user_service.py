adb95df6a81a2819528cb60160e71c8cd9f6525b
salut <Vous@exemple.com>
2025-02-12 22:06:22 +0100

    ♻️ BACKEND: application to backend to help ensai's students figureout

---
+++ backend/services/user_service.py
@@ -0,0 +1,22 @@
+from backend.business_object.user import User
+from backend.dao.user_dao import UserDAO
+from backend.dto.user_dto import UserDTO
+
+
+class UserService:
+    def __init__(self, user_dao: UserDAO):
+        self.user_dao = user_dao
+
+    def peut_se_connecter(self, user: User) -> bool:
+        """Vérifie si l'utilisateur peut se connecter (uniquement pour les administrateurs)."""
+        return user.is_admin()
+
+    def get_user(self, user_dto: UserDTO):
+        user = user_dto.to_user()
+        updated_user = self.user_dao.get_user_by_username(user.username)
+        return updated_user
+
+    @staticmethod
+    def of_context():
+        user_dao = UserDAO.of_context()
+        return UserService(user_dao=user_dao)
