2dd0fc255ffb19f2e8e2100881eaffdb5f430db8
salut <Vous@exemple.com>
2025-02-23 11:32:59 +0100

    ajout environnement pour lisibilité environnement et contrôles userlist

---
+++ frontend/src/components/Environnement.jsx
@@ -0,0 +1,8 @@
+const Environnement = () => {
+    const environnement = import.meta.env.VITE_ENVIRONNEMENT || "local"
+    return <div>
+        Environnement de déploiement : {environnement}
+    </div>
+}
+
+export default Environnement;
