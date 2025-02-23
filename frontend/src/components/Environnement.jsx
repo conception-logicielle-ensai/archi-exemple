25fbc4d9e9396580afe5979efc5dd836132feb71
ragatzino <antoine.brunetti@insee.fr>
2025-02-23 22:17:12 +0000

    typo in env

--- frontend/src/components/Environnement.jsx
+++ frontend/src/components/Environnement.jsx
@@ -1,6 +1,6 @@
 const Environnement = () => {
   const environnement = import.meta.env.VITE_ENVIRONNEMENT || "local";
-  const apiUrl = import.meta.env.VITE_URL_API || "localhost:8000";
+  const apiUrl = import.meta.env.VITE_API_URL || "localhost:8000";
   return (
     <div>
       <div>
